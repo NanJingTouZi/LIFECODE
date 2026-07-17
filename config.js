36b2e63e702692bb3e72043605d0d316f57d9e94
NanJing <isanwijaya82@gmail.com>
2026-07-18 00:21:29 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+apiKey: "AIzaSyDX9Rm_Wz8Ge2AiYvTMBQ4VbyQu6zI2esI",
+  authDomain: "shuyi-lifecode.firebaseapp.com",
+  projectId: "shuyi-lifecode",
+  storageBucket: "shuyi-lifecode.firebasestorage.app",
+  messagingSenderId: "386576582379",
+  appId: "1:386576582379:web:aa54c278e314361c1ee0ae",
+  measurementId: "G-CXEHC5EEMG"
+};
